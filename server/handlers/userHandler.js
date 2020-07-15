8db4ba50d6a7c83d89e3f5d9298395ad77f3b03c
kangmingtay <kang.ming1996@gmail.com>
2020-07-16 00:49:33 +0800

    add function to upload csv data to db

--- server/handlers/userHandler.js
+++ server/handlers/userHandler.js
@@ -57,6 +57,74 @@ const handleUpdateError = (res, err) => {
     });
 }
 
+async function handleUpdateRequest(res, fileData) {
+    const upsertQuery = `INSERT INTO users (id, login, name, salary) 
+        VALUES ($1, $2, $3, $4)
+        ON CONFLICT (id) DO UPDATE SET login = $2, name = $3, salary = $4 WHERE users.id = $1`;
+
+    const updateNameSalary = `UPDATE users SET name = $3, salary = $4 WHERE id = $1 AND login = $2`;
+    const getLoginConflict = `SELECT id, login FROM users WHERE login = $1`;
+    const updateLogin = `UPDATE users SET login = $2 WHERE id = $1`;
+    const getPreviousLogin = `SELECT login FROM users WHERE id = $1`;
+
+    const client = await pool.connect();
+    try {
+        let errors = [];
+        await client.query("BEGIN");
+        fileData.forEach(async row => {
+            const values = Object.values(row);
+
+            if (values[3] < 0) errors.push(Error("Salary cannot be negative!"));
+
+            let res = await client.query(updateNameSalary, values);
+            res = await client.query(getLoginConflict, [values[1]]);
+
+            const affectedRows = res.rows;
+            if (affectedRows.length !== 0) {
+
+                const tid = affectedRows[0].id;
+                const tlogin = affectedRows[0].login;
+                const pid = values[0];
+                if (pid !== tid) {
+                    res = await client.query(getPreviousLogin, [pid]);
+                    const plogin = res.rows[0].login;
+
+                    // swap logins here
+                    console.log(tid, plogin);
+                    res = await client.query(updateLogin, [tid, `tmp${plogin}`]);
+                    console.log(pid, tlogin);
+                    res = await client.query(updateLogin, [pid, tlogin]);
+                    console.log(tid, plogin);
+                    res = await client.query(updateLogin, [tid, plogin]);
+                } else {
+                    return
+                }
+            } else {
+                res = await client.query(upsertQuery, values);
+            }
+        });
+        if (errors.length !== 0) {
+            errors.forEach(err => {
+                throw err;
+            });
+        }
+        await client.query("COMMIT");
+    } catch (err) {
+        console.log("Rolling back...");
+        await client.query("ROLLBACK");
+        res.status(200).send({
+            success: false,
+            message: `Transaction failed: ${err.message}`,
+        });
+    } finally {
+        client.release();
+        res.status(200).send({
+            success: true,
+            message: "Database updated!",
+        });
+    }
+}
+
 async function handleUploadFileRequest(req, res) {
     let fileData = [];
     upload(req, res, async (err) => {
@@ -84,6 +152,7 @@ async function handleUploadFileRequest(req, res) {
                     handleUpdateError(res, Error("Cannot upload empty csv file!"));
                 } else {
                     // insert items into db here
+                    await handleUpdateRequest(res, fileData);
                 }
             });
         }
