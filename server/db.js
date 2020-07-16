ab9195ce63102160330b198af8b811bcbd78f70d
kangmingtay <kang.ming1996@gmail.com>
2020-07-17 07:21:07 +0800

    add function to initialise db on server start

--- server/db.js
+++ server/db.js
@@ -10,4 +10,23 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    client.query(`CREATE TABLE IF NOT EXISTS users(
+        id VARCHAR(100) PRIMARY KEY,
+        login VARCHAR(100), 
+        name VARCHAR(100),
+        salary NUMERIC(10, 2),
+        CONSTRAINT users_login UNIQUE(login) DEFERRABLE INITIALLY DEFERRED
+    )`, (err, res) => {
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        }
+        console.log('Database setup complete...');
+        release();
+    })
+})
+
 module.exports = pool;
