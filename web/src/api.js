96e698c6f0686a3dab048a52c05988e9897fe263
kangmingtay <kang.ming1996@gmail.com>
2020-07-19 02:09:09 +0800

    update api on web

--- web/src/api.js
+++ web/src/api.js
@@ -2,6 +2,10 @@ const BASE_URL = process.env.REACT_APP_API || "http://localhost:8888";
 
 export default {
     baseUrl: BASE_URL,
+    getEmployee: (id) => `${BASE_URL}/users/${id}`,
+    deleteEmployee: (id) => `${BASE_URL}/users/${id}`,
+    updateEmployee: (id) => `${BASE_URL}/users/${id}`,
+    createEmployee: (id) => `${BASE_URL}/users/${id}`,
     getEmployeesList: `${BASE_URL}/users`,
     uploadFile: `${BASE_URL}/users/upload`,
 }
